ff9fea65fe40e192fe73b4eba3d7fd048db4f18a
beki01 <rebeca.gonzalez@smoothstack.com>
2021-03-09 17:59:14 -0800

    Add AddMultipleValues.java file

---
+++ src/com/ss/march/jb/daytwo/AddMultipleValues.java
@@ -0,0 +1,34 @@
+package com.ss.march.jb.daytwo;
+
+import java.util.Scanner;
+
+public class AddMultipleValues {
+    /**
+     * Takes user inputs and displays final sum when a non numerical value is entered.
+     *
+     */
+    public static void main(String[] args){
+
+        Scanner userInput = new Scanner(System.in);
+
+        double currentAmt = 0, amtToAdd;
+        boolean isDouble = true;
+
+        System.out.println("Please enter initial numeric amount to add.");
+        System.out.println("(Use any non numerical character to display final calculation).");
+        do {
+            try {
+                amtToAdd = userInput.nextDouble();
+                currentAmt += amtToAdd;
+                System.out.println("Enter next amount.");
+
+            } catch (Exception e){
+                isDouble=false;
+                System.out.println("You have entered a non-numerical character.");
+                System.out.println("Calculated total is "+ currentAmt);
+            }
+        } while (isDouble);
+
+
+    }
+}
