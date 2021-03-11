dceef3917077d92b4a8029893fc5db337d7d3fdb
beki01 <rebeca.gonzalez@smoothstack.com>
2021-03-10 23:11:13 -0800

    Format PrintFileDirectory.java

--- src/com/ss/mar/jb/three/PrintFileDirectory.java
+++ src/com/ss/mar/jb/three/PrintFileDirectory.java
@@ -1,6 +1,5 @@
 package com.ss.mar.jb.three;
 
-
 import java.io.File;
 import java.util.Scanner;
 
@@ -8,9 +7,8 @@ import java.util.Scanner;
 public class PrintFileDirectory {
     /**
      * Prints file directory specified by user as a tree.
-     *
      */
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
         String filesPath;
         StringBuffer tree = new StringBuffer();
@@ -21,9 +19,9 @@ public class PrintFileDirectory {
         filesPath = userInput.nextLine();
         File directoryPath = new File(filesPath);
 
-        try{
+        try {
             getFolderTree(directoryPath, indent, tree);
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Incorrect Filepath. Please try again.");
             e.printStackTrace();
         }
@@ -34,42 +32,40 @@ public class PrintFileDirectory {
 
     /**
      * Builds tree directory from  using StringBuffer.
+     *
      * @param filePath - Filepath of directory to be displayed
-     * @param indent - Int value representing the initial indents on tree branch
-     * @param tree - StringBuffer where tree directory is saved to.
+     * @param indent   - Int value representing the initial indents on tree branch
+     * @param tree     - StringBuffer where tree directory is saved to.
      * @throws Exception - Throws exception if filepath is not a directory
      */
     public static void getFolderTree(File filePath, int indent, StringBuffer tree) throws Exception {
-        if (!filePath.isDirectory()){
+        if (!filePath.isDirectory()) {
             System.out.println("Filepath is not a Directory.");
             throw new Exception();
         }
         File[] fileLists = filePath.listFiles();
 
-        if(fileLists!=null){
+        if (fileLists != null) {
             tree.append(treeIndents(indent));
             tree.append("*--");
             tree.append(filePath.getName());
             tree.append("/");
             tree.append("\n");
             for (File file : fileLists) {
-                if (file.isDirectory()) {
-                    getFolderTree(file, indent + 1, tree);
-                } else {
-                    getFileBranch(file, indent + 1, tree);
-                }
-
+                if (file.isDirectory()) getFolderTree(file, indent + 1, tree);
+                else getFileBranch(file, indent + 1, tree);
             }
         }
     }
 
     /**
      * Appends File Name as branch to tree directory
+     *
      * @param branch - File name
      * @param indent - how deep the file is embedded from original filepath
-     * @param tree - StringBuffer of current tree directory
+     * @param tree   - StringBuffer of current tree directory
      */
-    public static void getFileBranch(File branch, int indent, StringBuffer tree){
+    public static void getFileBranch(File branch, int indent, StringBuffer tree) {
         tree.append(treeIndents(indent));
         tree.append("--");
         tree.append(branch.getName());
@@ -78,10 +74,11 @@ public class PrintFileDirectory {
 
     /**
      * Adds to tree buffer the number of indents representing how nested file is in from filepath
+     *
      * @param indent - how deep the file is embedded from original filepath
      * @return string to show how embedded a file is.
      */
-    public static String treeIndents(int indent){
+    public static String treeIndents(int indent) {
         return "| ".repeat(Math.max(0, indent));
     }
 
